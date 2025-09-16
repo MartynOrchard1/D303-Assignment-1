b766280a62e6821015fbcdbc2123e621c53e42c5
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-09-16 18:54:51 +1200

    #1 Added appropriate NuGet Packages

--- TuckBox/TuckBox/MauiProgram.cs
+++ TuckBox/TuckBox/MauiProgram.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Logging;
+﻿using CommunityToolkit.Maui; 
+
 
 namespace TuckBox
 {
@@ -9,15 +10,12 @@ namespace TuckBox
             var builder = MauiApp.CreateBuilder();
             builder
                 .UseMauiApp<App>()
+                .UseMauiCommunityToolkit()
                 .ConfigureFonts(fonts =>
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
                 });
-
-#if DEBUG
-    		builder.Logging.AddDebug();
-#endif
 
             return builder.Build();
         }
