53daabedb08045d19c8fe7fa6ecdb9b00a83f8b2
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-09-16 19:22:09 +1200

    Add Order model with properties and foreign keys
    Introduces the Order class to represent orders in the system, including properties for order ID, date, quantity, and foreign key references to related entities such as Food_Extra_Details, City, TimeSlot, and User.

---
+++ TuckBox/TuckBox/Models/Order.cs
@@ -0,0 +1,32 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuckBox.Models
+{
+    internal class Order
+    {
+        [PrimaryKey]
+        public string Order_ID { get; set; } = Guid.NewGuid().ToString();
+
+        public DateTime Order_Date { get; set; } = DateTime.UtcNow;
+        public int Quantity { get; set; } = 1;
+
+        // FK's - Just id's
+        [Indexed]
+        public string Food_Extra_DetailsFood_Details_ID { get; set; } = string.Empty; // -> Food_Extra_Details
+
+        [Indexed]
+        public string CityCity_ID { get; set; } = string.Empty; // -> City
+
+        [Indexed]
+        public string TimeSlotsTime_Slot_ID { get; set; } = string.Empty; // -> TimeSlot
+
+        [Indexed]
+        public string UserUser_ID { get; set; } = string.Empty; // -> User
+
+    }
+}
