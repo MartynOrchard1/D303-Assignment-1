908b947504642a778ea9af290962a112dbe623c4
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-09-16 19:13:29 +1200

    Adjust models to match erd

---
+++ TuckBox/TuckBox/Models/Food.cs
@@ -0,0 +1,15 @@
+﻿using SQLite;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TuckBox.Models
+{
+    internal class Food
+    {
+        [PrimaryKey]
+        public int Food_Id { get; set; }
+      
+}
