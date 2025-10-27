435c7662416cbb441ad5da673a63125e70a7bbb5
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-10-28 10:20:55 +1300

    Update MainPage.xaml.cs

--- TuckBox/TuckBox/MainPage.xaml.cs
+++ TuckBox/TuckBox/MainPage.xaml.cs
@@ -3,7 +3,28 @@ using TuckBox.Services;
 
 public partial class MainPage : ContentPage
 {
-    public MainPage() => InitializeComponent();
 
+    private readonly FirebaseDbService _dbService;
 
+    public MainPage(FirebaseDbService dbService)
+    {
+        InitializeComponent();
+        _dbService = dbService;
+
+        LoadCities();
+    }
+
+    private async void LoadCities()
+    {
+        try
+        {
+            var cities = await _dbService.GetCitiesAsync();
+            foreach (var city in cities.Values)
+                System.Diagnostics.Debug.WriteLine($"City: {city.City_Name}");
+        }
+        catch (Exception ex)
+        {
+            System.Diagnostics.Debug.WriteLine($"[ERROR] Firebase fetch failed: {ex.Message}");
+        }
+    }
 }
