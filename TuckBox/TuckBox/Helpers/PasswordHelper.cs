5d2001950e87d4c6436690febe01f9a3850a795f
MartynOrchard1 <121004839+MartynOrchard1@users.noreply.github.com>
2025-10-29 10:58:41 +1300

    Update PasswordHelper.cs

--- TuckBox/TuckBox/Helpers/PasswordHelper.cs
+++ TuckBox/TuckBox/Helpers/PasswordHelper.cs
@@ -9,6 +9,22 @@ namespace TuckBox.Helpers
 {
     internal class PasswordHelper
     {
+        public static string HashPassword(string password)
+        {
+           if (string.IsNullOrWhiteSpace(password))
+            {
+                throw new ArgumentException("Password cannot be null or empty.");
+            }
+            using var sha256 = SHA256.Create();
+            var bytes = Encoding.UTF8.GetBytes(password);
+            var hash = sha256.ComputeHash(bytes);
+            return Convert.ToBase64String(hash);
+        }
 
+        public static bool VerifyPassword(string password, string hashedPassword)
+        {
+            var hashOfInput = HashPassword(password);
+            return hashofEntered == storedHash;
+        }
     }
 }
